971d256ae24546ed2e12302f528ce183f073c260
hertz315 <hertz315@gmail.com>
2022-10-30 15:43:54 +0900

    initial Commit

---
+++ CheckTodos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckTodos
+//
+//  Created by Hertz on 10/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
